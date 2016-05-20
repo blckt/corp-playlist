ddfb90de41be3ed60f93313e3541ecda38f59661
Alexei Darmin <alexei.darmin@gmail.com>
2016-05-20 15:31:20 -0400

    Parts from koa-redux-react boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # koa-spa
-A Postgres backed Koa server with a React Redux single page application boilerplate. With Docker.
+A Sequelize backed Koa server with a React Redux single page application boilerplate. With Docker.
+
+Work in Progress - Coming Soon :) (will be tagged once functional)
+
+Some workflows are inspired by mezod's [koa redux boilerplate](https://github.com/mezod/boilerplate-koa-redux-react) however koa-spa is a full-stack boilerplate that includes an API layer, an ORM layer, connections to redux, continuous integration and more...
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var webpack = require('webpack');
+
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+  devtool: 'source-map',
+  entry: [
+    path.resolve(ROOT_PATH, 'app/src/index'),
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loaders: ['react-hot', 'babel']
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style','css','sass']
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: path.resolve(ROOT_PATH, 'app/build'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(ROOT_PATH, 'app/build'),
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlwebpackPlugin({
+      title: 'Listlogs'
+    })
+  ]
+};
